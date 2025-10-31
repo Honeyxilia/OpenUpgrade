1ed8f0727c1b257323ba3a7aef0431433f280159
Lena MONTENOT <lena.montenot@gmail.com>
2025-10-31 09:05:07 +0100

    rating: Removed score halving method in rating migration script
    Since Commown still uses the NPS rating system for project tasks, which is based on a 0-10 scoring system,
    we disabled this halving of rating scores operation, to keep the same range for project.task records.
    
    Note: this will still affect project.issue (outdated model) and mail.channel (livechat) rating records.

--- openupgrade_scripts/scripts/rating/14.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/rating/14.0.1.0/pre-migration.py
@@ -1,7 +1,11 @@
 # Copyright 2021 Tecnativa - Pedro M. Baeza
 # Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import logging
+
 from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
 
 
 def update_rating_value(env):
@@ -11,7 +15,9 @@ def update_rating_value(env):
         """
         UPDATE rating_rating
         SET rating = round(rating / 2.0)
-        WHERE rating IS NOT NULL""",
+        WHERE rating IS NOT NULL
+        AND res_model != 'project.task'
+        """,
     )
 
 
@@ -19,5 +25,9 @@ def update_rating_value(env):
 def migrate(env, version):
     openupgrade.rename_xmlids(
         env.cr, [("rating.action_view_rating", "rating.rating_rating_view")]
+    )
+    _logger.info(
+        "[Commown OpenUpgrade fork - rating pre-migration]"
+        " Score halving disabled for project.task ratings"
     )
     update_rating_value(env)
